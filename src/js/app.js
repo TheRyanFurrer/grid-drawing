51d2ae80225b414637f4c125f09385a6ef2c786e
Ryan Furrer <rfurrertech@gmail.com>
2021-12-22 22:30:23 -0500

    Swap click for "mouseup"

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,15 @@
 function paintGrid() {
   let grid = document.querySelector('#grid-container');
 
-  grid.addEventListener('click', event => {
+  // grid.addEventListener('click', event => {
+  //   if (event.target.className === 'grid-block unfilled') {
+  //     event.target.classList.add('filled');
+  //   } else if (event.target.className === 'grid-block unfilled filled') {
+  //     event.target.classList.remove('filled');
+  //   }
+  // });
+
+  grid.addEventListener('mouseup', event => {
     if (event.target.className === 'grid-block unfilled') {
       event.target.classList.add('filled');
     } else if (event.target.className === 'grid-block unfilled filled') {
