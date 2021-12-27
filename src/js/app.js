3d531946349637f9fc01933d7a9b8e51a8a75fd6
Ryan Furrer <rfurrertech@gmail.com>
2021-12-27 14:40:35 -0500

    Began adding reset button

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 function paintGrid() {
   let grid = document.querySelector('#grid-container');
 
-  grid.addEventListener('click', event => {
+  grid.addEventListener('click', (event) => {
     if (event.target.className === 'grid-block unfilled') {
       event.target.classList.add('filled');
     } else if (event.target.className === 'grid-block unfilled filled') {
@@ -10,5 +10,20 @@ function paintGrid() {
   });
 }
 
-paintGrid();
+function resetGrid() {
+  const reset = document.querySelector('#reset-btn');
+  let filledBlocks = document.querySelectorAll('.filled');
 
+  reset.addEventListener('click', (event) => {
+    if (event.target.id === 'reset-btn') {
+      for (let elem of filledBlocks) {
+        elem.classList.remove('filled');
+
+        console.log('reset');
+      }
+    }
+  });
+}
+
+paintGrid();
+resetGrid();
